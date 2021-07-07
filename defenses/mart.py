0d9a97f32bed9ce9a2c68ec555300f42285b3e59
theFool32 <saber.rl32@gmail.com>
2021-07-07 11:20:37 +0800

    :ambulance: API update for trades and mart

--- defenses/mart.py
+++ defenses/mart.py
@@ -10,7 +10,7 @@ from losses import get_loss_fn
 
 
 class MART(TRADES):
-    def __init__(self, _model, _attack, **kw):
-        super(MART, self).__init__(_model, _attack, **kw)
+    def __init__(self, _model, _attack, _args):
+        super(MART, self).__init__(_model, _attack, _args)
         self.inner_loss_fn = get_loss_fn("CE")
         self.outer_loss_fn = get_loss_fn("mart_outer")
